b1182b0639d1f22b88b5468349089002e2c4a310
Вадим Горин <eruell@yandex.ru>
2018-08-05 18:25:45 +0300

    auth views \ AuthMe integration \ Usertable migration

---
+++ app/Providers/Eloquent/EloquentUserNickProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Providers\Eloquent;
+
+use Illuminate\Auth\EloquentUserProvider;
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+use App\Hashers\AuthMeHasher;
+
+class EloquentUserNickProvider extends EloquentUserProvider
+{
+    public function validateCredentials(UserContract $user, array $credentials)
+    {
+        $plain = $credentials['password'];
+
+        return ( new AuthMeHasher() )->check($plain, $user->getAuthPassword());
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,30 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->unique();
             $table->string('password');
+
+            $table->string('email')->unique()->nullable();
+            $table->string('nick')->nullable();
+
+            $table->boolean('is_logged')->nullable();
+
+            $table->string('has_session')->nullable();
+
+            $table->ipAddress('ip')->nullable();
+            $table->ipAddress('reg_ip')->nullable();
+
+            $table->bigInteger('last_login')->nullable();
+            $table->bigInteger('reg_date')->nullable();
+
+            $table->double('last_x')->nullable();
+            $table->double('last_y')->nullable();
+            $table->double('last_z')->nullable();
+
+            $table->string('last_world', 100)->nullable();
+            $table->float('last_yaw')->nullable();
+            $table->float('last_pitch')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
